92c218fc28ab4facdc8031d07b246496134c2b9c
DionisSerbin <reaganusm@gmail.com>
2023-02-11 13:59:38 +0300

    navigation

--- app/src/main/java/smart/estate/app/presentation/MainActivity.kt
+++ app/src/main/java/smart/estate/app/presentation/MainActivity.kt
@@ -2,11 +2,25 @@ package smart.estate.app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import smart.estate.app.R
+import smart.estate.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
     }
 }
